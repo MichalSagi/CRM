7381f50dfa9377763fa23dfb17537d1793420723
MichalSagi <smitzi@gmail.com>
2020-10-18 18:47:51 +0300

    n

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const Sequelize = require('sequelize')
 const api = require("./server/routes/api");
 
 const app = express();
-const sequelize = new Sequelize(process.env.CLEARDB_DATABASE_URL || 'mysql://root:@localhost/crm_db')
+const sequelize = new Sequelize(process.env.CLEARDB_DATABASE_URLno || 'mysql://root:@localhost/crm_db')
 
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
@@ -16,6 +16,7 @@ app.use(function(req, res, next) {
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use("/", api);
+// app.use('/', express.static(path.join(__dirname, 'dist')))
 
 const PORT = 8080;
 app.listen(process.env.PORT || PORT, () => console.log(`Running server on port ${PORT}`));
